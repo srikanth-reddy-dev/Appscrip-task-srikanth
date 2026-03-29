4012c61703a1912d63becdf093be4852201cdad8
Srikanth Reddy <thathireddyvenkatasrikanthredd@gmail.com>
2026-03-29 19:02:40 +0530

    fix: handle params + prevent server crash

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -4,27 +4,31 @@ async function getProduct(id: string) {
   });
 
   if (!res.ok) {
-    throw new Error("Failed to fetch product");
+    return null;
   }
 
   return res.json();
 }
 
-export default async function ProductPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const product = await getProduct(params.id);
+export default async function ProductPage({ params }: any) {
+  const id = params?.id;
+
+  if (!id) {
+    return <h1>Invalid Product ID</h1>;
+  }
+
+  const product = await getProduct(id);
+
+  
+  if (!product) {
+    return <h1>Product not found</h1>;
+  }
 
   return (
     <div style={{ padding: "20px" }}>
       <h1>{product.title}</h1>
-
       <img src={product.image} width={200} alt={product.title} />
-
       <p>{product.description}</p>
-
       <h3>₹ {product.price}</h3>
     </div>
   );
